a40f3e9e64c8271c293f098254dda3c9d054e4ca
Zalina Kodz <kod3ik87@gmail.com>
2023-05-31 00:53:07 +0300

    changed steps

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import helpers.Attach;
@@ -10,22 +9,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static config.WebConfigForProject.config;
 
 
 
 public class TestBase {
-    static Faker faker = new Faker();
-    static String CaseName = faker.name().fullName();
-    static String newTestCaseName = faker.gameOfThrones().quote();
-    static String testCaseName = faker.name().fullName();
-    static  String testStepName1 = faker.shakespeare().asYouLikeItQuote();
-    static String testStepName2 = faker.shakespeare().asYouLikeItQuote();
-    static String testStepName3 = faker.shakespeare().asYouLikeItQuote();
+ Faker faker = new Faker();
+ String CaseName = faker.name().fullName();
+ String newTestCaseName = faker.gameOfThrones().quote();
+ String testCaseName = faker.name().fullName();
+ String testStepName1 = faker.shakespeare().asYouLikeItQuote();
+ String testStepName2 = faker.shakespeare().asYouLikeItQuote();
+ String testStepName3 = faker.shakespeare().asYouLikeItQuote();
 
 
     public static String projectId = "2211",
-            allureTestOpsSession = "b1b37ac1-ba73-4715-a836-cbb1fb5bf49e",
+            allureTestOpsSession = "e6b20b21-6a15-4931-9043-1e370e9befd8",
             token = "e96309d1-e0a7-428e-9155-f0f226a0ef7c";
 
     @BeforeAll
@@ -44,9 +44,9 @@ public class TestBase {
         Attach.screenshotAs("Last step screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        closeWebDriver();
         if (config.isRemote()) {
             Attach.addVideo();
         }
-        Selenide.closeWebDriver();
     }
 }
